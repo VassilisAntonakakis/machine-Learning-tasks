c3ba4c8562b3b11b6f05f8d721f8dcaa51741bb4
VassilisAntonakakis <94934935+VassilisAntonakakis@users.noreply.github.com>
2022-05-23 18:49:12 +0300

    scatter works for task 2

--- task2.py
+++ task2.py
@@ -1,7 +1,7 @@
 from calendar import c
 import string
 import numpy as np
-from numpy import float64
+from numpy import delete, float64
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
@@ -20,24 +20,40 @@ def int_mapper(df, target_column):
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 data = pd.read_csv("task2trainning.csv", delimiter=";")
 dfTrain = pd.DataFrame(data)
+dfTrain = pd.DataFrame(data)
+del data
+data = pd.read_csv("task2eval.csv", delimiter=";")
+dfEval = pd.DataFrame(data)
 
-#dfTrain["alcohol"] = pd.to_numeric(dfTrain["alcohol"], errors = 'coerce')
-
-index = 0
-for col in dfTrain:
-    if dfTrain[col].isnull().values.any():
-        print("NaN value at: ", col, " at index: ", index)
-    index += 1
 
 kmeans = KMeans(n_clusters = 10, random_state = 0)
 kmeans.fit(dfTrain[["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol"]])
 
-label = kmeans.predict(dfTrain[["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol"]])
+labelEval = kmeans.predict(dfEval[["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol"]])
+
+index = 1
+for cluster in labelEval:
+    print(index, " belongs to cluster: " , cluster)
+    index += 1
+
 
 #plotting the results:
-u_labels = np.unique(label)
+u_labels = np.unique(labelEval)
+centroids = kmeans.cluster_centers_
+
 for i in u_labels:
-    plt.scatter(dfTrain[label == i , 0] , dfTrain[label == i , 1] , label = i)
+    plt.scatter(dfEval[["fixed acidity"]],
+    dfEval[["volatile acidity"]],
+    c = labelEval,
+    s = 50,
+    cmap = 'viridis'
+    )
 
 plt.legend()
 plt.show()
+
+#for i in u_labels:
+#   plt.scatter(dfEval[label == i , 0] , dfEval[label == i , 1] , label = i)
+
+#plt.legend()
+#plt.show()
